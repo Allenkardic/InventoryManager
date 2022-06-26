84502754aaba1202ec1ff34fad8c9d88e10cc2df
aliakwe <aliakwe@yahoo.com>
2022-06-26 12:44:19 +0100

    implement asyc storage testing to ensure CRUD

--- jest/setup.js
+++ jest/setup.js
@@ -7,4 +7,33 @@ jest.mock('@react-navigation/native', () => ({
   },
 }));
 
-jest.mock('@react-native-async-storage/async-storage', () => 'Asyncstore');
+jest.mock('@react-native-async-storage/async-storage', () => {
+  let cache = {};
+  return {
+    setItem: jest.fn((key, value) => {
+      return new Promise((resolve, reject) => {
+        return typeof key !== 'string' || typeof value !== 'string'
+          ? reject(new Error('key and value must be string'))
+          : resolve((cache[key] = value));
+      });
+    }),
+    getItem: jest.fn((key, value) => {
+      return new Promise(resolve => {
+        return cache.hasOwnProperty(key) ? resolve(cache[key]) : resolve(null);
+      });
+    }),
+    removeItem: jest.fn(key => {
+      return new Promise((resolve, reject) => {
+        return cache.hasOwnProperty(key)
+          ? resolve(delete cache[key])
+          : reject('No such key!');
+      });
+    }),
+    clear: jest.fn(key => {
+      return new Promise((resolve, reject) => resolve((cache = {})));
+    }),
+    getAllKeys: jest.fn(key => {
+      return new Promise((resolve, reject) => resolve(Object.keys(cache)));
+    }),
+  };
+});
